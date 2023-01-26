23a28ad88685c7063031c0603f20c0b4e02a2065
tomjes03 <sonofdsonofgod@yahoo.com>
2023-01-26 03:53:20 +0100

    My Alx backend task commit message

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,5 @@
+// Modify the following function’s budget object to simply use the keyname instead.
+const getBudgetObject = (income, gdp, capita) => {
+  return { income, gdp, capita };
+}
+export default getBudgetObject
