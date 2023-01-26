0830448dc69624becae5b6bbb475369655844401
tomjes03 <sonofdsonofgod@yahoo.com>
2023-01-26 03:46:58 +0100

    My Alx Backend task commit message

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+// Condense the internals of the following function to 1 line - without
+// changing the name of each function/variable
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
