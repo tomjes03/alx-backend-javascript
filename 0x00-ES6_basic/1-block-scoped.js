23c01455732698f5f2c1c560e18f384ec73d58f1
tomjes03 <sonofdsonofgod@yahoo.com>
2023-02-04 23:02:45 +0000

    My Alx Backend Javascript task commit message

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
-  task = false;
-  task2 = true;
+
   return [task, task2];
 }
