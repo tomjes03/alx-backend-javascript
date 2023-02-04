1c91439b0ef092194fb89c605b37e7e682877e37
tomjes03 <sonofdsonofgod@yahoo.com>
2023-02-04 23:09:21 +0000

    My Alx Backend Javascript task commit message

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,12 @@
-// function to use ES6 computed property names on the budget object
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-  
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
+  return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
-  return budget;
 }
