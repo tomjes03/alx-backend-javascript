4f73a430aa3e25893a7ae42c72439321b85fb12e
tomjes03 <sonofdsonofgod@yahoo.com>
2023-02-05 16:44:00 +0000

    My Alx Backend Javascript task commit messagé

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,10 @@
-//  function named createReportObject whose parameter, employeesList,
-// is the return value of the previous function createEmployeesObject.
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,
+    allEmployees: {
+      ...employeesList,
+    },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
 }
-
