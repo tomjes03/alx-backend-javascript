6d3bb0dfb4bd7e4183dbc9d34c9347b60e4488f9
tomjes03 <sonofdsonofgod@yahoo.com>
2023-02-05 16:50:03 +0000

    My ALx Backend Javascript task commit message

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,3 @@
-//  function named iterateThroughObject. The function’s parameter
-// reportWithIterator is the return value from createIteratorObject.
-// return every employee name in a string, separated by |
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  return [...reportWithIterator].join(' | ');
 }
-
